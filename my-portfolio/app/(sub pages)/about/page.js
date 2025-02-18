124a6447252a142042564fe11ed4c5a37b1a3095
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-02-18 18:07:53 -0500

    about page responsive

--- my-portfolio/app/(sub pages)/about/page.js
+++ my-portfolio/app/(sub pages)/about/page.js
@@ -15,15 +15,15 @@ export default function Home() {
         className="-z-10 h-full w-full fixed object-cover object-center top-0 left-0" // Places the image behind other content
       />
 
-      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0 ">
+      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 ">
         <RenderModel>
           <HelmetModel />
         </RenderModel>
       </div>
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-6xl text-accent">Aziz A. Kabia</h1>
+        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold text-4xl xs:text-5xl  lg:text-6xl text-accent">Aziz A. Kabia</h1>
           <p className="font-light text-foreground text-base ">
             Meet the space traveler behind this portfolio{" "}
           </p>
