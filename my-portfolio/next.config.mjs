cfaead80371d805f4c9ee55b1310cdd077859e1c
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-03-06 18:35:04 -0500

    prep for deployment

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Required for GitHub Pages
+    images: {
+      unoptimized: true, // GitHub Pages does not support Next.js Image Optimization
+    },
+    basePath: '/BC-React-App', // Use your repo name here
+    assetPrefix: '/BC-React-App/', // Ensures assets load correctly
+  };
 
 export default nextConfig;
